ae88cbde35f997eed9f3bbbfb83b0add059ad82f
Mirco Dotta <mirco.dotta@typesafe.com>
2012-11-20 11:22:32 +0100

    System-agnostic script for building the IDE and its dependendies.
    * Check executables are available.
    * Clone repositories in current folder.
    * Keystore password not shown in the terminal.
    * Allow to select the original repositories or the scala-ide forks
      for building scala-refactoring and scalariform.
    * Allow to select the branch (or tag) to be built for scala-ide, scalariform,
      and scala-refactoring.
    * Allow to skip tests when building scala-refactoring. You need to
      set REFACTORING_MAVEN_ARGS=-Dmaven.test.skip=true when launching
      the script.
    * Added support for SCALA_VERSION 2.11.0-SNAPSHOT

--- build-full-ide.sh
+++ build-full-ide.sh
@@ -1,89 +1,27 @@
 #!/bin/bash -e
 
-# Build IDE and all it's dependencies
+# Warning: This script has been tested wit Mac OSX only.
 
-ECLIPSE=/Applications/Programming/eclipse-indigo/eclipse
 
-# Password to the keystore
-KEYSTORE_PASS=
+###############################################################
+#                       SCALA VERSION                         #
+###############################################################
 
-# Command to run SBT
-sbt="xsbt"
-
-## Where does each module reside on the filesystem, relative to the current directory?
-
-SCALAIDE_DIR=dragos-scala-ide
-SCALARIFORM_DIR=scalariform
-SCALA_REFACTORING_DIR=scala-refactoring
-SBINARY_DIR=sbinary
-SBT_DIR=xsbt
-TYPESAFE_KEYSTORE=typesafe-keystore
-
-if [[ -z $VERSION_TAG ]]; then
-	VERSION_TAG=local
+if [[ -z "$SCALA_VERSION" ]]
+then
+  echo "SCALA_VERSION cannot be empty"
+  aborting
 fi
 
-REPO_SUFFIX=210x
-LOCAL_REPO=`pwd`/m2repo
-
-base_dir=`pwd`
-
-# What files can have uncommitted changes?
-IGNORED_FILES_REGEX='plugin-signing\|\.classpath'
-
-function validate_java()
-{
-	(java -version 2>&1 | grep \"1.6.*\")
-	if [[ $? -ne 0 ]]; then
-		echo -e "Invalid Java version detected. Only java 1.6 is supported due to changes in jarsigner in 1.7\n"
-		java -version
-		exit 1
-	fi
-}
-
-#
-# Check that there are no uncommitted changes in $1
-# 
-function validate()
-{
-  (
-  	cd $1
-	git diff --name-only | grep -v ${IGNORED_FILES_REGEX} > /dev/null
-  )
-  RET=$?
-  if [[ $RET -eq 0 ]]; then
-  	echo -e "\nYou have uncommitted changes in $1:\n"
-  	(cd $1 && git diff --name-status | grep -v ${IGNORED_FILES_REGEX})
-  	echo -e "\nAborting mission."
-  	exit 1
-  fi 
-}
-
-function print_step()
-{
-	cat <<EOF
-
-==================================================================
-                     Building $1
-==================================================================
-
-EOF
-}
-
-set_version()
-{
-    mvn -f pom.xml -N versions:set -DnewVersion=$1
-    mvn -f pom.xml -N versions:update-child-modules
-}
-
-function describe()
-{
-	(cd $1 && git describe --all)
-}
-
-############## Diagnostics ###################
 
 case $SCALA_VERSION in
+
+	2.9* )
+		maven_toolchain_profile=sbt-2.9
+		scala_profile_ide=scala-2.9.x
+		REPO_SUFFIX=29x
+		;;
+
 	2.10.0-SNAPSHOT )
 		maven_toolchain_profile=sbt-2.10
 		scala_profile_ide=scala-2.10.x
@@ -102,11 +40,10 @@ case $SCALA_VERSION in
 		REPO_SUFFIX=210x
 		;;
 
-
-	2.9* )
-		maven_toolchain_profile=sbt-2.9
-		scala_profile_ide=scala-2.9.x
-		REPO_SUFFIX=29x
+    2.11.0-SNAPSHOT )
+		maven_toolchain_profile=sbt-2.11
+		scala_profile_ide=scala-2.11.x
+		REPO_SUFFIX=211x
 		;;
 
 	*)
@@ -114,25 +51,148 @@ case $SCALA_VERSION in
 		exit 1
 esac
 
+###############################################################
+#                          Global Methods                     #
+###############################################################
+
+function aborting()
+{
+  echo "Aborting."
+  exit 1
+}
+
+function print_step()
+{
+	cat <<EOF
+
+==================================================================
+                     Building $1
+==================================================================
+
+EOF
+}
+
+# Check that the VERSION_TAG was provided. If not, abort.
+function assert_version_tag_not_empty()
+{
+if [[ -z "$VERSION_TAG" ]]
+  then
+    echo "VERSION_TAG cannot be empty."
+    aborting
+  fi
+}
+
+###############################################################
+#                           Global Variables                  #
+###############################################################
+
+: ${ECLIPSE:=eclipse} # ECLIPSE will take the declared value if not overridden
+: ${SBT:=sbt}         # SBT will take the declared value if not overridden
+MAVEN=mvn
+: ${REFACTORING_MAVEN_ARGS:=""} # Pass some maven argument to the build, e.g. -Dmaven.test.skip=true
+export MAVEN_OPTS="-Xmx1500m"
+GIT=git
+KEYTOOL=keytool       # Needed for signing the JARs
+
+base_dir=`pwd`
+
+###############################################################
+#      Checks that the needed executables are available       #
+###############################################################
+
+function validate_java()
+{
+	(java -version 2>&1 | grep \"1.6.*\")
+	if [[ $? -ne 0 ]]; then
+		echo -e "Invalid Java version detected. Only java 1.6 is supported due to changes in jarsigner in 1.7\n"
+		java -version
+		exit 1
+	fi
+}
+
+# If passed executable's name is available return 0 (true), else return 1 (false).
+# @param $1 The executable's name
+function executable_in_path() 
+{
+  COMMAND=$1
+  RES=$(command -v $COMMAND)
+  if [ "$RES" ]
+  then
+    echo "$COMMAND is available in the PATH."
+    return 0
+  else
+    return 1
+  fi
+}
+
+# Exit with code failure 1 if the executable is not available.
+# @param $1 The executable's name
+function assert_executable_in_path() 
+{
+  COMMAND=$1
+  (executable_in_path $COMMAND) || {
+    echo >&2 "Couldn't find $COMMAND in the PATH."
+    aborting
+  }
+}
+
+# Checks that all executables needed by this script are available
+assert_executable_in_path ${MAVEN} # Check that maven executable is available
+assert_executable_in_path ${ECLIPSE} # Check that eclipse executable is available
+assert_executable_in_path ${GIT} # Check that git executable is available
+assert_executable_in_path ${SBT} # Check that sbt executable is available
+
+validate_java
+
+###############################################################
+#                          SIGNING                            #
+###############################################################
+
+KEYSTORE_FOLDER="typesafe-keystore"
+KEYSTORE_GIT_REPO=
+KEYSTORE_PASS=
+
+read -n1 -p "Would you like to sign the IDE? [y/Y/n/N] " signing; echo
+
+if [[ "$signing" == "y" ||  "$signing" == "Y" ]]
+then
+  assert_executable_in_path ${KEYTOOL} # Check that keytool executable is available
+  assert_version_tag_not_empty
+
+  # Check if the keystore folder has been already pulled
+  if [ ! -d "$KEYSTORE_FOLDER" ]
+  then
+    read -p "Please, provide the URL to the keystore git repository: " git_repo; echo
+    KEYSTORE_GIT_REPO="$git_repo"
+    checkout_git_repo $KEYSTORE_GIT_REPO $KEYSTORE_FOLDER
+  fi
+  
+  # Password for using the keystore
+  read -s -p "Please, provide the password for the keystore: " passw; echo
+  KEYSTORE_PASS=$passw
+  # Check that the password to the keystore is correct (or fail fast)
+  $KEYTOOL -list -keystore ${base_dir}/${KEYSTORE_FOLDER}/typesafe.keystore -storepass ${KEYSTORE_PASS} -alias typesafe
+else
+  echo "The IDE build will NOT be signed."
+  if [[ -z $VERSION_TAG ]]; then
+	VERSION_TAG=local
+  fi
+fi
+
+############## Helpers #######################
+
+SCALAIDE_DIR=scala-ide
+SCALARIFORM_DIR=scalariform
+SCALA_REFACTORING_DIR=scala-refactoring
+SBINARY_DIR=sbinary
+SBT_DIR=xsbt
+
+
+LOCAL_REPO=`pwd`/m2repo
 SOURCE=${base_dir}/p2-repo
 PLUGINS=${SOURCE}/plugins
 REPO_NAME=scala-eclipse-toolchain-osgi-${REPO_SUFFIX}
 REPO=file:${SOURCE}/${REPO_NAME}
-
-echo -e "Build configuration:"
-echo -e "-----------------------\n"
-echo -e "Sbt: \t\t\t${sbt}"
-echo -e "Scala version: \t\t${SCALA_VERSION}"
-echo -e "Version tag: \t\t${VERSION_TAG}"
-echo -e "P2 repo: \t\t${SOURCE}"
-echo -e "Toolchain repo: \t${REPO}"
-
-echo -e "Scala IDE:  \t\t`describe dragos-scala-ide`"
-echo -e "Scalariform:\t\t`describe scalariform`"
-echo -e "Scala-refactoring:\t`describe scala-refactoring`"
-echo -e "-----------------------\n"
-
-############## Helpers #######################
 
 function build_sbinary()
 {
@@ -142,7 +202,7 @@ function build_sbinary()
 	cd ${SBINARY_DIR}
 
 	# maven style for the toolchain build
-	$sbt "reboot full" clean "show scala-instance" "set every crossScalaVersions := Seq(\"${SCALA_VERSION}\")" \
+	$SBT "reboot full" clean "show scala-instance" "set every crossScalaVersions := Seq(\"${SCALA_VERSION}\")" \
 	 'set every publishMavenStyle := true' \
 	 'set every resolvers := Seq("Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots")' \
 	 'set every publishTo := Some(Resolver.file("Local Maven",  new File(Path.userHome.absolutePath+"/.m2/repository")))' \
@@ -160,7 +220,7 @@ function build_xsbt()
 	print_step "xsbt"
 
 	cd ${SBT_DIR}
-	$sbt "reboot full" clean \
+	$SBT "reboot full" clean \
 	"set every crossScalaVersions := Seq(\"${SCALA_VERSION}\")" \
 	'set every publishMavenStyle := true' \
 	'set every resolvers := Seq("Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots")' \
@@ -222,7 +282,7 @@ function build_refactoring()
 
 	cd ${SCALA_REFACTORING_DIR}
 	GIT_HASH="`git log -1 --pretty=format:"%h"`"
-	${MAVEN} -Dscala.version=${SCALA_VERSION} -P ${scala_profile_ide} -Drepo.scala-ide="file:/${SOURCE}" -Dmaven.test.skip=true -Dgit.hash=${GIT_HASH} clean package
+	${MAVEN} -P ${scala_profile_ide} -Dscala.version=${SCALA_VERSION} $REFACTORING_MAVEN_ARGS -Drepo.scala-ide="file:/${SOURCE}" -Dgit.hash=${GIT_HASH} clean package
 
 	cd $base_dir
 
@@ -257,7 +317,7 @@ function build_scalariform()
 
 	GIT_HASH="`git log -1 --pretty=format:"%h"`"
 	
-	${MAVEN} -Dscala.version=${SCALA_VERSION} -P ${scala_profile_ide} -Drepo.scala-ide="file:/${SOURCE}" -Dgit.hash=${GIT_HASH} clean package
+	${MAVEN} -P ${scala_profile_ide} -Dscala.version=${SCALA_VERSION} -Drepo.scala-ide="file:/${SOURCE}" -Dgit.hash=${GIT_HASH} clean package
 
 	rm -rf ${SOURCE}/scalariform-${REPO_SUFFIX}
 	mkdir ${SOURCE}/scalariform-${REPO_SUFFIX}
@@ -277,22 +337,221 @@ function build_ide()
 
 function sign_plugins()
 {
-	print_step "Signing"
+    print_step "Signing"
+    
 	cd ${SCALAIDE_DIR}/org.scala-ide.sdt.update-site
-	./plugin-signing.sh ${base_dir}/${TYPESAFE_KEYSTORE}/typesafe.keystore typesafe ${KEYSTORE_PASS} ${KEYSTORE_PASS}
-	cd ${base_dir}
+	ECLIPSE_ALIAS=$ECLIPSE
+	ECLIPSE=$(which $ECLIPSE_ALIAS) ./plugin-signing.sh ${base_dir}/${KEYSTORE_FOLDER}/typesafe.keystore typesafe ${KEYSTORE_PASS} ${KEYSTORE_PASS}
+    cd ${base_dir}
 }
 
-############## Build #########################
 
-export MAVEN_OPTS="-Xmx1500m"
+###############################################################
+#                          GIT Helpers                        #
+###############################################################
 
-validate_java
-validate ${SBINARY_DIR}
-validate ${SBT_DIR}
-validate ${SCALARIFORM_DIR}
-validate ${SCALA_REFACTORING_DIR}
-validate ${SCALAIDE_DIR}
+function clone_git_repo_if_needed()
+{
+  GITHUB_REPO=$1
+  FOLDER_DIR=$2
+  NAME_REMOTE=$3
+
+  if [ ! -d "$FOLDER_DIR" ]
+  then
+    if [[ -z "$NAME_REMOTE" ]]
+    then
+      # Cloning as "origin"
+      $GIT clone $GITHUB_REPO $FOLDER_DIR
+    else
+      # Cloning as "$NAME_REMOTE"
+      $GIT clone $GITHUB_REPO $FOLDER_DIR -o $NAME_REMOTE
+    fi
+  else
+    cd $FOLDER_DIR
+    # If a remote with name "$NAME_REMOTE" doesn't exists yet, then add it
+    REMOTES=`$GIT remote show | awk '/'$NAME_REMOTE'/ {print $1}'`
+    if [[ -z "$REMOTES" ]]
+    then
+      git remote add $NAME_REMOTE $GITHUB_REPO
+    fi
+    # In all cases, make sure to bring all changes locally
+    git fetch $NAME_REMOTE
+    cd $base_dir
+  fi
+}
+
+function exist_branch_in_repo()
+{
+  BRANCH=$1
+  GIT_REPO=$2
+  
+  ESCAPED_BRANCH=`echo $BRANCH | sed -e 's/[\/&]/\\\&/g'`
+  # Checks if it exists a remote branch that matches ESCAPED_BRANCH
+  REMOTES=`$GIT ls-remote $GIT_REPO | awk '/'$ESCAPED_BRANCH'/ {print $2}'`
+  if [[ "$REMOTES" ]]
+  then
+    return 0
+  else
+    return 1
+  fi 
+}
+
+function exist_branch_in_repo_verbose()
+{
+  BRANCH=$1
+  GIT_REPO=$2
+  
+  echo "Checking if branch $BRANCH exists in git repo ${GIT_REPO}..."
+  if exist_branch_in_repo $BRANCH $GIT_REPO
+  then
+    echo "Branch found!"
+    return 0
+  else
+    echo "Branch NOT found!"
+    return 1
+  fi
+}
+
+function assert_branch_in_repo_verbose()
+{
+  BRANCH=$1
+  GIT_REPO=$2
+  (exist_branch_in_repo_verbose $BRANCH $GIT_REPO) || aborting
+}
+
+# Check that there are no uncommitted changes in $1
+function validate()
+{
+  IGNORED_FILES_REGEX='\.classpath'
+  (
+  	cd $1
+	$GIT diff --name-only | grep -v ${IGNORED_FILES_REGEX} > /dev/null
+  )
+  RET=$?
+  if [[ $RET -eq 0 ]]; then
+  	echo -e "\nYou have uncommitted changes in $1:\n"
+  	(cd $1 && git diff --name-status | grep -v ${IGNORED_FILES_REGEX})
+  	echo -e "\nAborting mission."
+  	exit 1
+  fi 
+}
+
+function checkout_git_repo()
+{
+  GITHUB_REPO=$1
+  FOLDER_DIR=$2
+  BRANCH=$3
+  REMOTE_NAME=$4
+
+  FULL_BRANCH_NAME=$BRANCH
+  if [[ "$REMOTE_NAME" ]]
+  then
+    FULL_BRANCH_NAME= "$REMOTE_NAME/$BRANCH"
+  fi
+
+  cd $FOLDER_DIR
+	
+  REFS=`$GIT show-ref $BRANCH | awk '{split($0,a," "); print a[2]}' | awk '{split($0,a,"/"); print a[2]}'`
+  if [[ "$REFS" = "tags" ]]
+  then
+    echo "In $FOLDER_DIR, checking out tag $BRANCH"
+    git checkout $BRANCH
+  else
+    echo "In $FOLDER_DIR, checking out branch $FULL_BRANCH_NAME"
+    $GIT checkout $FULL_BRANCH_NAME
+  fi
+  cd $base_dir
+  validate ${FOLDER_DIR}
+}
+
+###############################################################
+#                            BUILD                            #
+###############################################################
+
+# At this point the version tag cannot be empty. Why? Because if the IDE build won't be signed, 
+# then VERSION_TAG is set to `local`. Otherwise, if the IDE build will be signed, then the 
+# VERSION_TAG must be set or the build will stop immediately.
+# This is really just a sanity check.
+assert_version_tag_not_empty
+
+# Selecting Git repositories
+
+REMOTE_NAME=origin # Name of the remote
+
+# These are currently non-overridable defaults
+SBINARY_GIT_REPO=git://github.com/scala-ide/sbinary.git
+SBT_GIT_REPO=git://github.com/harrah/xsbt.git
+SCALA_IDE_GIT_REPO=git://github.com/scala-ide/scala-ide.git
+
+# Defaults can be changed
+SCALARIFORM_GIT_REPO=git://github.com/mdr/scalariform.git
+SCALA_REFACTORING_GIT_REPO=git://git.assembla.com/scala-refactoring.git
+
+read -n1 -p "Do you want to build the IDE dependencies using the original repositories, or the GitHub forks under the scala-ide organization? (o/f): " original_or_fork; echo;
+case "$original_or_fork" in
+	o ) 
+		echo "Using the original repositories"
+		;;
+		
+	f ) 
+		echo "Using the GitHub forks for $SCALARIFORM_DIR and $SCALA_REFACTORING_DIR"
+		REMOTE_NAME=fork
+		SCALARIFORM_FORK_GIT_REPO=git://github.com/scala-ide/scalariform.git
+		SCALA_REFACTORING_FORK_GIT_REPO=git://github.com/scala-ide/scala-refactoring.git
+		SCALARIFORM_GIT_REPO=$SCALARIFORM_FORK_GIT_REPO
+		SCALA_REFACTORING_GIT_REPO=$SCALA_REFACTORING_FORK_GIT_REPO
+		;;
+		
+	*)
+		echo "Unexpected input"
+		aborting
+		;;
+esac
+
+clone_git_repo_if_needed ${SBINARY_GIT_REPO} ${SBINARY_DIR}
+clone_git_repo_if_needed ${SBT_GIT_REPO} ${SBT_DIR}
+clone_git_repo_if_needed ${SCALA_IDE_GIT_REPO} ${SCALAIDE_DIR}
+clone_git_repo_if_needed ${SCALARIFORM_GIT_REPO} ${SCALARIFORM_DIR} $REMOTE_NAME
+clone_git_repo_if_needed ${SCALA_REFACTORING_GIT_REPO} ${SCALA_REFACTORING_DIR} $REMOTE_NAME
+
+
+# Selecting branches/tags to build
+
+SBT_BRANCH=0.13
+SBINARY_BRANCH=master
+
+read -p "What branch/tag should I use for building the ${SCALAIDE_DIR}: " scala_ide_branch;
+assert_branch_in_repo_verbose $scala_ide_branch $SCALA_IDE_GIT_REPO
+
+
+read -p "What branch/tag should I use for building ${SCALARIFORM_DIR}: " scalariform_branch;
+assert_branch_in_repo_verbose $scalariform_branch $SCALARIFORM_GIT_REPO
+
+read -p "What branch/tag should I use for building ${SCALA_REFACTORING_DIR}: " scala_refactoring_branch;
+assert_branch_in_repo_verbose $scala_refactoring_branch $SCALA_REFACTORING_GIT_REPO
+
+
+echo -e "Build configuration:"
+echo -e "-----------------------\n"
+echo -e "Sbt: \t\t\t${SBT}"
+echo -e "Scala version: \t\t${SCALA_VERSION}"
+echo -e "Version tag: \t\t${VERSION_TAG}"
+echo -e "P2 repo: \t\t${SOURCE}"
+echo -e "Toolchain repo: \t${REPO}"
+
+echo -e "SBinary:\t\t${SBINARY_DIR}, branch: ${SBINARY_BRANCH}, repo: ${SBINARY_GIT_REPO}"
+echo -e "Sbt:\t\t\t${SBT_DIR}, branch: ${SBT_BRANCH}, repo: ${SBT_GIT_REPO}"
+echo -e "Scalariform:\t\t${SCALARIFORM_DIR}, branch: ${scalariform_branch}, repo: ${SCALARIFORM_GIT_REPO}"
+echo -e "Scala-refactoring:\t${SCALA_REFACTORING_DIR}, branch: ${scala_refactoring_branch}, repo: ${SCALA_REFACTORING_GIT_REPO}"
+echo -e "Scala IDE:  \t\t${SCALAIDE_DIR}, branch: ${scala_ide_branch}, repo: ${SCALA_IDE_GIT_REPO}"
+echo -e "-----------------------\n"
+
+
+checkout_git_repo ${SBINARY_GIT_REPO} ${SBINARY_DIR} ${SBINARY_BRANCH}
+checkout_git_repo ${SBT_GIT_REPO} ${SBT_DIR} ${SBT_BRANCH}
+checkout_git_repo ${SCALA_IDE_GIT_REPO} ${SCALAIDE_DIR} ${scala_ide_branch}
+checkout_git_repo ${SCALARIFORM_GIT_REPO} ${SCALARIFORM_DIR} ${scalariform_branch} $REMOTE_NAME
+checkout_git_repo ${SCALA_REFACTORING_GIT_REPO} ${SCALA_REFACTORING_DIR} ${scala_refactoring_branch} $REMOTE_NAME
 
 build_sbinary
 build_xsbt
@@ -300,4 +559,8 @@ build_toolchain
 build_refactoring
 build_scalariform
 build_ide
-sign_plugins
+
+if [[ "$signing" == "y" ||  "$signing" == "Y" ]]
+then
+  sign_plugins
+fi
